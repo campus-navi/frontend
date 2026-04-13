68efe5656e7f88a52e200e017bb93cc010c12bec
SeongWoo Shin <asdfgg77@naver.com>
2026-04-13 18:01:41 +0900

    feat: 반응형 온보딩 흐름 구현 (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { AppLayout } from '@/components/layout/AppLayout';
-import AboutPage from '@/pages/AboutPage';
-import HomePage from '@/pages/HomePage';
+import OnboardingPage from '@/pages/OnboardingPage';
+import LoginPage from '@/pages/LoginPage';
+import SignupPage from '@/pages/SignupPage';
 
 export default function App() {
   return (
     <Routes>
-      <Route element={<AppLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="about" element={<AboutPage />} />
-      </Route>
+      <Route index element={<OnboardingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
       <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
   );
